125d93dcc6ba12508709f4c7451529431b3c379e
Mateus <mateus.vitor.andrade@gmail.com>
2026-06-07 13:48:10 -0300

    cria context da snackbar

--- dt-money-app/App.tsx
+++ dt-money-app/App.tsx
@@ -1,12 +1,15 @@
 import './src/styles/global.css'
 
 import { AuthContextProvider } from '@/context/auth.context'
+import { SnackbarContextProvider } from '@/context/snackbar.context'
 import NavigationRoutes from '@/routes'
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <NavigationRoutes />
-    </AuthContextProvider>
+    <SnackbarContextProvider>
+      <AuthContextProvider>
+        <NavigationRoutes />
+      </AuthContextProvider>
+    </SnackbarContextProvider>
   )
 }
