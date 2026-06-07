b93050b27d426544d919b8aa05ba97b4ad3a9f62
Mateus <mateus.vitor.andrade@gmail.com>
2026-06-07 13:28:17 -0300

    usa o usuario do contexto nas rotas

--- dt-money-app/src/routes/index.tsx
+++ dt-money-app/src/routes/index.tsx
@@ -1,20 +1,21 @@
+import { useAuthContext } from '@/context/auth.context'
 import { NavigationContainer } from '@react-navigation/native'
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 import { SystemBars } from 'react-native-edge-to-edge'
 
 import { PrivateRoutes } from './PrivateRoutes'
 import { PublicRoutes } from './PublicRoutes'
 
 const NavigationRoutes = () => {
-  const [user, setUser] = useState(undefined)
+  const { user, token } = useAuthContext()
 
   const Routes = useCallback(() => {
-    if (!user) {
+    if (!user || !token) {
       return <PublicRoutes />
-    } else {
-      return <PrivateRoutes />
     }
-  }, [user])
+
+    return <PrivateRoutes />
+  }, [user, token])
 
   return (
     <NavigationContainer>
