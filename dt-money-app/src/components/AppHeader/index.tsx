b97ed007249c941b11f96b101cfae916a8ed9f3a
Mateus <mateus.vitor.andrade@gmail.com>
2026-06-07 14:12:54 -0300

    cria o cabecalho da home

---
+++ dt-money-app/src/components/AppHeader/index.tsx
@@ -0,0 +1,32 @@
+import { useAuthContext } from '@/context/auth.context'
+import { colors } from '@/shared/colors'
+import { MaterialIcons } from '@expo/vector-icons'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+
+export const AppHeader = () => {
+  const { handleLogout } = useAuthContext()
+
+  return (
+    <View className="w-full flex-row p-8 justify-between">
+      <View>
+        <Image
+          source={require('@/assets/logo.png')}
+          className="w-[130px] h-[30px]"
+        />
+
+        <TouchableOpacity
+          className="flex-row items-center gap-2 mt-2"
+          onPress={handleLogout}
+        >
+          <MaterialIcons name="logout" color={colors.gray[700]} size={15} />
+
+          <Text className="text-gray-700 text-base">Sair da conta</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity className="bg-accent-brand w-[130px] items-center justify-center rounded-xl h-[50px]">
+        <Text className="text-white font-bold text-sm">Nova transação</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
--- dt-money-app/src/screens/Home/index.tsx
+++ dt-money-app/src/screens/Home/index.tsx
@@ -1,14 +1,10 @@
-import { useAuthContext } from '@/context/auth.context'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { AppHeader } from '@/components/AppHeader'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const Home = () => {
-  const { handleLogout } = useAuthContext()
-
   return (
-    <View>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Sair</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <AppHeader />
+    </SafeAreaView>
   )
 }
