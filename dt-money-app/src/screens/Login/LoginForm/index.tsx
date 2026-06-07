357db45f543061a0df93c20b8cec6169f79a4608
Mateus <mateus.vitor.andrade@gmail.com>
2026-06-07 11:40:18 -0300

    finaliza interface da tela de login

--- dt-money-app/src/screens/Login/LoginForm/index.tsx
+++ dt-money-app/src/screens/Login/LoginForm/index.tsx
@@ -1,5 +1,6 @@
 import { AppButton } from '@/components/AppButton'
 import { AppInput } from '@/components/AppInput'
+import { Text, View } from 'react-native'
 import { useForm } from 'react-hook-form'
 
 export interface FormLoginParams {
@@ -33,7 +34,17 @@ export const LoginForm = () => {
         secureTextEntry
       />
 
-      <AppButton iconName="arrow-forward">Login</AppButton>
+      <View className="flex-1 justify-between mt-8 mb-8 min-h-[250px]">
+        <AppButton iconName="arrow-forward">Login</AppButton>
+
+        <View>
+          <Text className="mb-6 text-gray-300 text-base">
+            Ainda não possui uma conta?
+          </Text>
+
+          <AppButton mode="outline">Cadastrar</AppButton>
+        </View>
+      </View>
     </>
   )
 }
